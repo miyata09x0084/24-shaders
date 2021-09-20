b94d92199f3ba402193a570885136317f4a04569
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2021-09-20 09:13:38 +0900

    Initial commit

--- src/script.js
+++ src/script.js
@@ -1,129 +1,73 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+// import * as dat from 'dat.gui'
 import testVertexShader from './shaders/test/vertex.glsl'
 import testFragmentShader from './shaders/test/fragment.glsl'
 
-/**
- * Base
- */
-// Debug
-const gui = new dat.GUI()
+import matcap from '../static/textures/hotspot.png'
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+var container;
+var camera, scene, renderer, clock;
+var uniforms;
 
-// Scene
-const scene = new THREE.Scene()
+container = document.getElementById( 'container' );
 
-/**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader()
-const flagTexture = textureLoader.load('/textures/flag-french.jpg')
+camera = new THREE.Camera();
+camera.position.z = 1;
 
-/**
- * Test mesh
- */
-// Geometry
-const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
+scene = new THREE.Scene();
+clock = new THREE.Clock();
 
-const count = geometry.attributes.position.count
-const randoms = new Float32Array(count)
+var geometry = new THREE.PlaneBufferGeometry( 2, 2 );
 
-for(let i = 0; i < count; i++)
-{
-    randoms[i] = Math.random()
-}
+uniforms = {
+    time: { type: "f", value: 1.0 },
+    matcap: { value: new THREE.TextureLoader().load(matcap) },
+    resolution: { type: "v2", value: new THREE.Vector2() },
+    mouse: { type: "v2", value: new THREE.Vector2(0, 0) }
+};
 
-geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1))
-
-// Material
-const material = new THREE.ShaderMaterial({
+var material = new THREE.ShaderMaterial( {
+    uniforms: uniforms,
     vertexShader: testVertexShader,
-    fragmentShader: testFragmentShader,
-    uniforms:
-    {
-        uFrequency: { value: new THREE.Vector2(10, 5) },
-        uTime: { value: 0 },
-        uColor: { value: new THREE.Color('green')},
-        uTexture: { value: flagTexture }
+    fragmentShader: testFragmentShader
+} );
+
+var mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio( window.devicePixelRatio );
+
+container.appendChild( renderer.domElement );
+
+onWindowResize();
+window.addEventListener( 'resize', onWindowResize, false );
+
+const mouse = new THREE.Vector2();
+window.addEventListener('mousemove', (e) => {
+    mouse.x = e.pageX / width - 0.5;
+    mouse.y = - e.pageY / height + 0.5;
+})  
+
+function onWindowResize( event ) {
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    uniforms.resolution.value.x = renderer.domElement.width;
+    uniforms.resolution.value.y = renderer.domElement.height;
+}
+    
+
+function render() {
+
+    uniforms.time.value += clock.getDelta();
+    if(mouse) {
+        uniforms.mouse.value = mouse;    
     }
-})
 
-gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX')
-gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY')
+    renderer.render( scene, camera );
 
-// Mesh
-const mesh = new THREE.Mesh(geometry, material)
-mesh.scale.y = 2 / 3
-scene.add(mesh)
-
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+    window.requestAnimationFrame(render);
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0.25, - 0.25, 1)
-scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
-const clock = new THREE.Clock()
-
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-
-    //Update materials
-    material.uniforms.uTime.value = elapsedTime
-
-    // Update controls
-    controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
+render();
--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,14 +1,180 @@
-uniform vec3 uColor;
-uniform sampler2D uTexture;
-
-varying vec2 vUv;
-varying float vElavation;
-
-void main()
+uniform float time;
+uniform sampler2D matcap;
+uniform vec2 mouse;
+uniform vec2 resolution;
+float PI = 3.141592653589793238;
+mat4 rotationMatrix(vec3 axis, float angle) {
+    axis = normalize(axis);
+    float s = sin(angle);
+    float c = cos(angle);
+    float oc = 1.0 - c;
+    
+    return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+                0.0,                                0.0,                                0.0,                                1.0);
+}
+vec2 getMatcap(vec3 eye, vec3 normal) {
+  vec3 reflected = reflect(eye, normal);
+  float m = 2.8284271247461903 * sqrt( reflected.z+1.0 );
+  return reflected.xy / m + 0.5;
+}
+vec3 rotate(vec3 v, vec3 axis, float angle) {
+	mat4 m = rotationMatrix(axis, angle);
+	return (m * vec4(v, 1.0)).xyz;
+}
+// polynomial smooth min (k = 0.1);
+float smin( float a, float b, float k )
 {
-    vec4 textureColor = texture2D(uTexture, vUv);
-    textureColor.rgb *= vElavation * 2.0 + 0.5;
-    gl_FragColor = textureColor;
+    float h = clamp( 0.5+0.5*(b-a)/k, 0.0, 1.0 );
+    return mix( b, a, h ) - k*h*(1.0-h);
+}
 
-    gl_FragColor = vec4(vUv, 0.76, 1.0);
+// //行列による回転
+// vec3 rotate(vec3 p,float radX,float radY,float radZ){
+//     mat3 mx = mat3(
+//         1.0,0.0,0.0,
+//         0.0,cos(radX),-sin(radX),
+//         0.0,sin(radX),cos(radX)
+//     );
+//     mat3 my = mat3(
+//         cos(radY),0.0,sin(radY),
+//         0.0,1.0,0.0,
+//         -sin(radY),0.0,cos(radY)
+//     );
+//     mat3 mz = mat3(
+//         cos(radZ),-sin(radZ),0.0,
+//         sin(radZ),cos(radZ),0.0,
+//         0.0,0.0,1.0
+//     );
+//     return mx * my * mz * p;
+// }
+
+// //球形に座標アニメーション
+// vec3 sphericalPolarCoord(float radius, float rad1, float rad2){
+//     return vec3(
+//         sin(rad1) * cos(rad2) * radius,
+//         sin(rad1) * sin(rad2) * radius,
+//         cos(rad1) * radius
+//     );
+// }
+
+// //スムーズに結合するための補間
+// float smoothMin(float d1, float d2, float k){
+//     float h = exp(-k * d1) + exp(-k * d2);
+//     return -log(h) / k;
+// }
+
+//球体の距離関数
+float distanceFuncSphere(vec3 p,float r){
+    return length(p) - r;
+}
+
+//箱の距離関数
+float sdBox( vec3 p, vec3 b )
+{
+  vec3 q = abs(p) - b;
+  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
+}
+
+float sdf(vec3 p){
+    vec3 p1 = rotate(p, vec3(1.), time/2.);
+    float box = sdBox(p1, vec3(0.25));
+    float sphere = distanceFuncSphere(p - vec3(mouse*resolution, 0.), 0.2);
+    return smin(box, sphere, 0.1);
+}
+
+// //距離関数
+// float distanceFunc(vec3 p){
+//     float n1 = snoise(p * 0.3 + time / 100.0);
+
+//     vec3 p1 = rotate(p,radians(time),radians(time),radians(time));
+//     vec3 s1 = sphericalPolarCoord(3.0,radians(time),radians(-time * 2.0));
+//     float d1 = distanceFuncSphere(p1+s1,1.25) - n1 * 0.25;
+
+//     vec3 p2 = rotate(p,radians(time),radians(time),radians(time));
+//     vec3 s2 = sphericalPolarCoord(3.0,radians(-time * 5.0),radians(-time));
+//     float d2 = distanceFuncSphere(p2+s2,1.25) - n1 * 0.25;
+
+//     vec3 p3 = rotate(p,radians(time),radians(time),radians(time));
+//     vec3 s3 = sphericalPolarCoord(3.0,radians(time),radians(-time * 5.0));
+//     float d3 = distanceFuncSphere(p3+s3,1.25) - n1 * 0.25;
+
+//     return smoothMin(smoothMin(d1,d2,2.0),d3,2.0);
+// }
+
+//distanceFunc
+vec3 getNormal(vec3 p)
+{
+    float d = 0.0001; // dy dx
+    return normalize(vec3(
+        sdf(p + vec3(d, 0.0, 0.0)) - sdf(p + vec3(-d, 0.0, 0.0)),
+        sdf(p + vec3(0.0, d, 0.0)) - sdf(p + vec3(0.0, -d, 0.0)),
+        sdf(p + vec3(0.0, 0.0, d)) - sdf(p + vec3(0.0, 0.0, -d))
+    ));
+}
+
+void main( void ) {
+    vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
+
+    vec3 camPos = vec3(0.0, 0.0, 1.0); //Camera Position
+    vec3 camDir = vec3(0.0, 0.0, -1.0); //Camera Front Direction
+    vec3 camUp = vec3(0.0, 1.0, 0.0); //Camera Up Direction
+    vec3 camSide = cross(camDir, camUp); //Camera Side Direction
+
+    float ta = 1.; //Traget Depth
+
+    vec3 ray = normalize(camSide * p.x + camUp * p.y + camDir * ta);
+
+    float dis = 0.0; //Minimum distance btween the ray and the object
+    float rLen = 0.0; //Length to add to the ray
+    vec3 rayPos = camPos;
+    float t = 0.;
+    float tMax = 5.;
+
+    //マーチングループ
+    for(int i = 0; i < 256; i++)
+    {
+        // dis = distanceFunc(rPos);
+        // rLen += dis;
+
+        // rPos = cPos + ray * rLen; 
+        vec3 pos = camPos + t * ray; //Current Position
+        float h = sdf(pos);
+        if(h<0.001 || t>tMax) break;
+        t += h;
+    }
+
+    // vec3 normal = getNormal(rPos);
+
+    vec3 color = vec3(0.);
+    //Collide Check
+    if (t < tMax){
+        vec3 pos = camPos + t * ray;
+        color = vec3(1.);
+        vec3 normal = getNormal(pos);
+        color = normal;
+        float diff = dot(vec3(1.), normal);
+        vec2 matcapUV = getMatcap(ray, normal);
+        color = vec3(diff);
+        color = texture2D(matcap, matcapUV).rgb;
+    }
+
+    gl_FragColor = vec4(color, 1.);
+    // {
+    //     float n = snoise(rPos * 0.2 + time / 100.0);
+    //     vec3 p = rotate(rPos,radians(time * -2.0),radians(time * 2.0),radians(time * -2.0));
+    //     float d = distanceFuncSphere(p,1.6) - n;
+
+    //     if(d > 2.0){
+    //         gl_FragColor = vec4(hsv(dot(normal,cUp) * 0.8 + time / 200.0, 0.2, dot(normal,cUp) * 0.6 + 0.6), 1.0);
+    //     }else if(d < 2.0 && d > 1.0){
+    //         gl_FragColor = vec4(hsv(dot(normal,cUp) * 0.1 + time / 100.0, 0.8, dot(normal,cUp) * 0.3 + 0.8), 1.0);
+    //     }else{
+    //         gl_FragColor = vec4(hsv(dot(normal,cUp) * 0.8 + time / 200.0,0.2,dot(normal,cUp) * 0.6 + 0.5), 1.0);
+    //     }
+    // }else 
+    // {
+    //     gl_FragColor = vec4(vec3(0.0), 1.0);
+    // }
 }
